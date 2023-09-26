a1746a533b197428fce6f072d49753257c9046cd
Laondohemba <125279970+Laondohemba@users.noreply.github.com>
2023-09-26 15:05:35 +0100

    Add files via upload

--- includes/js/script.js
+++ includes/js/script.js
@@ -108,6 +108,61 @@ document.getElementById("buy_now_btn12").addEventListener("click", function(){
     buyNow(product12, cost12);
 });
 
+// product 13
+let product13 = document.getElementById("product13");
+let cost13 = document.getElementById("price13");
+
+document.getElementById("buy_now_btn13").addEventListener("click", function(){
+    buyNow(product13, cost13);
+});
+
+// product 14
+let product14 = document.getElementById("product14");
+let cost14 = document.getElementById("price14");
+
+document.getElementById("buy_now_btn14").addEventListener("click", function(){
+    buyNow(product14, cost14);
+});
+// product 15
+let product15 = document.getElementById("product15");
+let cost15 = document.getElementById("price15");
+
+document.getElementById("buy_now_btn15").addEventListener("click", function(){
+    buyNow(product15, cost15);
+});
+
+// product 16
+let product16 = document.getElementById("product16");
+let cost16 = document.getElementById("price16");
+
+document.getElementById("buy_now_btn16").addEventListener("click", function(){
+    buyNow(product16, cost16);
+});
+
+// product 17
+let product17 = document.getElementById("product17");
+let cost17 = document.getElementById("price17");
+
+document.getElementById("buy_now_btn17").addEventListener("click", function(){
+    buyNow(product17, cost17);
+});
+
+// product 18
+let product18 = document.getElementById("product18");
+let cost18 = document.getElementById("price18");
+
+document.getElementById("buy_now_btn18").addEventListener("click", function(){
+    buyNow(product18, cost18);
+});
+
+// product 19
+let product19 = document.getElementById("product19");
+let cost19 = document.getElementById("price19");
+
+document.getElementById("buy_now_btn19").addEventListener("click", function(){
+    buyNow(product19, cost19);
+});
+
 // unisex wears button
 
 document.getElementById("unisex_btn").addEventListener("click", function(){
